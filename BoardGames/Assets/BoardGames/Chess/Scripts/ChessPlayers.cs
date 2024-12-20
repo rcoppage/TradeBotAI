b61ceb167d7228bce1c9992cb611aaa589dcae64
Robert R Coppage <107591986+rcoppage@users.noreply.github.com>
2024-12-20 17:32:06 -0500

    more changes for spawning chess pieces

--- BoardGames/Assets/BoardGames/Chess/Scripts/ChessPlayers.cs
+++ BoardGames/Assets/BoardGames/Chess/Scripts/ChessPlayers.cs
@@ -16,4 +16,61 @@ public class ChessPlayers : MonoBehaviour
     private string player; 
 
     //Refrences for all sprites of chess pieces
+    public Sprite black_queen, black_king, black_knight, black_bishop, black_rook, black_pawn;
+    public Sprite white_queen, white_king, white_knight, white_bishop, white_rook, white_pawn;
+
+    public void Activate() {
+
+        controller = GameObject.FindGameObjectWithTag("GameController");
+
+        //take the instantiated location and adjust the transform
+        SetCoords();
+
+        //Sets chesspiece object to correct image based opn name
+        switch (this.name) {
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
+            
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
+        }
+    }
+
+    public void SetCoords() {
+        float x = xBoard;
+        float y = yBoard;
+
+        x *= 0.66f;
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+
+        this.transform.position = new Vector3(x, y, -1.0f);
+    }
+
+    //Getters and setters
+    public int GetXBoard() {
+        return xBoard; 
+    }
+
+    public int GetYBoard() {
+        return yBoard;
+    }
+
+    public void setXBoard(int x) {
+        xBoard = x;
+    }
+
+    public void SetYBoard(int y) {
+        yBoard = y;
+    }
 }
